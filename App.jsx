c8d08ccb484817e5ca64bae6f39555213ae305bb
berkantahme73-sudo <berkantahme73@gmail.com>
2026-04-25 19:33:59 +0300

    Аpp.jsx
    Всичките данни от файловете header, footer и filmName импортирах в главния файл Аpp

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import Header from "./Header.jsx"
+import Footer from "./Footer.jsx"
+import FilmName from "./FilmName.jsx"
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <FilmName></FilmName>
+      <Footer></Footer>
+
+    </>
+  )
+
+}
+
+export default App
