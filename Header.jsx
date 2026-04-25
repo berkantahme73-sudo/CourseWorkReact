8505672c10be347105ae066e8b05190cb33e7348
berkantahme73-sudo <berkantahme73@gmail.com>
2026-04-25 19:25:58 +0300

    Header
    Започнахме с Header. Направвих навигацията

---
+++ Header.jsx
@@ -0,0 +1,17 @@
+function Header(){
+    return(
+        <header>
+            <h1>Каталог за филми</h1>
+        <nav>
+            <ul>
+                 <li>Филми</li>
+                 <li>Жанр</li>
+            </ul>
+       
+    </nav>
+    <hr />
+    </header>
+    )
+    
+}
+export default Header;
