d8ea16013b6b9110547135fd17729197f65ab322
berkantahme73-sudo <berkantahme73@gmail.com>
2026-04-28 19:37:58 +0300

    Добавих връзка между страниците
    Чрез компонентите Router, routes и route добавихме връзки между страниците

--- App.jsx
+++ App.jsx
@@ -1,13 +1,27 @@
-import Header from "./Header.jsx"
-import Footer from "./Footer.jsx"
-import FilmName from "./FilmName.jsx"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/pages/Home";
+import SignUp from "./components/pages/SignUp"
+import Page2 from "./components/pages/page2";
+import Page3 from "./components/pages/page3";
+import Layout from "./components/Layout";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 function App() {
   return (
     <>
-      <Header></Header>
-      <FilmName></FilmName>
-      <Footer></Footer>
+      <Header />
+      <Router>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="page2" element={<Page2 />} />
+            <Route path="page3" element={<Page3 />} />
+            <Route path="signup" element={<SignUp />} />
+          </Route>
 
+        </Routes>
+      </Router>
+      <Footer />
     </>
   )
 
